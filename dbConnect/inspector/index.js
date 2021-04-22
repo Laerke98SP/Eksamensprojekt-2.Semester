b0fa3681f31df8262c2f45c73437475d341bef59
Lærke Stald Pedersen <laerke20ad@student.cbs.dk>
2021-04-22 09:52:31 +0200

    New structure and admin login created

---
+++ dbConnect/inspector/index.js
@@ -0,0 +1,64 @@
+const db = require('../shared/dbInspector');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.')
+
+    try {
+        await db.startDb(); //start db connection
+    } catch (error) {
+        console.log("Error connecting to the database", error.message)
+    }
+    switch (req.method) {
+        case 'GET':
+            await get(context, req);
+            break;
+        // case 'POST':
+        //     await post(context, req);
+        //     break
+        default:
+            context.res = {
+                body: "Please get or post"
+            };
+            break
+    }
+}
+
+async function get(context, req){
+    try{
+        let email = req.query.email;
+        let password = req.query.password;
+        console.log(email);
+        console.log(password);
+        let admin = await db.selectAdmin(email, password)
+        console.log("Executed to line 31 in azure function")
+
+        context.res = {
+            body: admin
+        };
+        console.log("also send the context to client side")
+    } catch(error){
+        context.res = {
+            status: 404,
+            body: `No admin - ${error.message}`
+        }
+    } 
+}
+
+// async function post(context, req){
+//     try{
+//         let payload = req.body;
+//         await db.insert(payload)
+//         context.res = {
+//             status: 200,
+//             body: {
+//                 status: 'Success'
+//             }
+//         }
+//     } catch(error){
+//         context.res = {
+//             status: 400,
+//             body: error.message
+//         }
+//     }
+// }
+
---
+++ dbConnect/userActions/index.js
@@ -0,0 +1,63 @@
+const db = require('../shared/dbUser');
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.')
+
+    try {
+        await db.startDb(); //start db connection
+    } catch (error) {
+        console.log("Error connecting to the database", error.message)
+    }
+    switch (req.method) {
+        case 'GET': 
+            await get(context, req);
+            break;
+        case 'POST':
+            await post(context, req);
+            break
+        default:
+            context.res = {
+                body: "Please get or post"
+            };
+            break
+    }
+}
+
+async function get(context, req){
+    try{
+        let email = req.query.email;
+        console.log(email);
+        let users = await db.selectAll(email)
+        console.log("Executed to line 32 in azure function")
+        
+        context.res = {
+            body: users
+        };
+        console.log("also send the context to client side")
+    } catch(error){
+        context.res = {
+            status: 404,
+            body: `No users - ${error.message}`
+        }
+    } 
+}
+
+async function post(context, req){
+    try{
+        let payload = req.body;
+        await db.userVote(payload)
+        context.res = {
+            status: 200,
+            body: {
+                status: 'Success'
+            }
+        }
+    } catch(error){
+        context.res = {
+            status: 400,
+            body: error.message
+        }
+    }
+}
+

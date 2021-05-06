93aeb0cc9dc6653d5719da2476b3b7803ec0dafe
lauraboejer <70098397+lauraboejer@users.noreply.github.com>
2021-05-06 12:33:06 +0200

    ændringer i match

--- Frontend/js/match.js
+++ Frontend/js/match.js
@@ -1,116 +1,136 @@
-// ---------------- DEFINING BUTTONS ------------------- //
-const like = document.getElementById("like");
-const dislike = document.getElementById("dislike");
-const deleteBtn = document.getElementById("delete");
+function showGender(gender) {
+    if (gender = 0) {
+        return gender = "Female"
+    } else if (gender = 1) {
+        return gender = "Male"
+    };
+};
 
-// ---------- SHOW DIV CONTAINERS FOR INNERHTML -------- //
-const table = document.getElementById("table")
-const username = document.getElementById("username")
-const password = document.getElementById("password")
-const firstname = document.getElementById("firstname")
-const lastname = document.getElementById("lastname")
-const age = document.getElementById("age")
-const gender = document.getElementById("gender")
-const minage = document.getElementById("minage")
-const maxage = document.getElementById("maxage")
-const pref = document.getElementById("pref")
-const descr = document.getElementById("description");
-const info = document.getElementById("info");
+function showGenderPref(genderPref) {
+    if (genderPref = 0) {
+        return genderPref = "Female"
+    } else if (genderPref = 1) {
+        return genderPref = "Male"
+    };
+};
 
-// ---------- Notification ----------- //
+function calculateAge(dob) {
+    var age = new Date(dob);
+    var diffMS = Date.now() - age.getTime(); //d.d. minus 'age', dsv. differencen udregnes gemmes som ny variabel
+    var ageDate = new Date(diffMS); //udregningen af differencen mellem d.d. og 'age' gemmes som ny dato
+        return dob = Math.abs(ageDate.getUTCFullYear() - 1970); // getUTCFullYear = omregner til et helt år minus 1970
+}; //ovenstående metode udregner objekternes alder ud fra attributterne 'birthYear', 'birthMonth' og birthDate.
 
 let note = document.getElementById('notification');
 
 // ----------------- The logged in users mail -----------//
-let email = localStorage.getItem('mail');
+let userEmail = localStorage.getItem('email');
 
 function checkIfMatch(){
-
-
     const option = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset-UTF-8'
         },
         body: JSON.stringify({
-            email: email
+            email: userEmail
         })
     };
-
     // Insert match if both user like eachother
     fetch(`http://localhost:7071/api/match`, option)
-     .then((response) => {
-            
-            return response.json()
-        })
-        .then((data) => {
-                // MISSING SOME IF ELSE STATEMENT TO CHECK FOR DIFFERENT ERRORS //
-                console.log("process Succeeded")
-                console.log(data)
-                if( data.status == 'Success' ){
-                    note.innerHTML += "You have a new match"
-                }
-            
-        
-        }).catch((err) =>{
-            console.log(err)
-            console.log("Something went wroooong")
-        })
-}
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+    // MISSING SOME IF ELSE STATEMENT TO CHECK FOR DIFFERENT ERRORS //
+        console.log("Process succeeded")
+        console.log(data)
+        if( data.status == 'Success' ){
+            note.innerHTML += "You have a new match"
+        } else {
+            note.innerHTML += "No new matches"
+        };
+        getMatches();
+    }).catch((err) =>{
+        console.log(err)
+        console.log("Something went wroooong")
+    });
+};
 
+// Lærkes kode!!
+function getMatches(){
+    // Getting the matches inked to the user email
+    fetch(`http://localhost:7071/api/match?email=${userEmail}`)
+    .then((response) => {
+        response.json();
+    }).then((matches) => {
+        console.log(matches);
+        // Creating a forloop that iterates through matches
+        for (i in matches){
+            // Saving the matched profiles email and saving the match id
+            var age = calculateAge(matches[i].dob);
 
+            // Creating full name
+            var fullName = matches[i].firstName + " " + matches[i].lastName;
 
+            // Calling matches function
+            showMatches(fullName, age, id)
+        };
+    });
+};
 
+function showMatches(name, age, className){
+    // Creating html elements
+    const holder = document.createElement('div');
+    const matchName = document.createElement('h2');
+    const matchAge = document.createElement('h4');
+    const deleteMatch = document.createElement('button');
 
+    // Inserting values in html elements
+    const nameTxt = document.createTextNode(name);
+    const ageTxt = document.createTextNode(age);
+    const buttonTxt = document.createTextNode('Slet match');
 
+    // Defining id's
+    holder.className = 'matchHolder';
+    matchName.className = 'matchName';
+    matchAge.className = 'matchAge';
+    deleteMatch.className = 'deleteMatch';
 
-// ------------ SHOW MATCHES ------------ //
-function displayMatch(data){
-        username.innerHTML = data[1].value 
-        password.innerHTML = data[2].value 
-        firstname.innerHTML = data[3].value 
-        lastname.innerHTML = data[4].value  
-        age.innerHTML = data[5].value 
-        gender.innerHTML = data[6].value 
-        
-        //localStorage.setItem('votedOn', data[1].value);
+    // Defining button id as match className
+    deleteMatch.id = className;
 
-        descr.innerHTML = data[7].value;
-    
-        minage.innerHTML = data[8].value 
-        maxage.innerHTML = data[9].value 
-        pref.innerHTML = data[10].value  
+    // Creating function that runs onClick
+    deleteMatch.onclick = function() {
+        // Getting the id from the classname
+        var id = deleteMatch.id
 
-}
-
-
-// ------------- DELETE MATCH ---------- //
-deleteBtn.addEventListener('click', function(){
-})
-
-function deleteMatch(){
-    if(localStorage.getItem('mail')){
-        const option = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(user)
-        };    
-           
-           
-        fetch(`http://localhost:7071/api/match?email=${mail}`, option)
-        .then(function() {
-            console.log("ok"); 
-            displayMatch(data);
-        }).catch(function() {
-            console.log("error"); 
+        // Creating API delete request
+        const options = { 
+            method: 'DELETE', 
+            headers: { 
+                'Content-type': 'application/json'
+            }
+        };
+        // Contacting match api with id
+        fetch(`http://localhost:5000/match/${id}`, options)
+        .then(function () {
+            // Delay function so data has time to update 
+            setTimeout(function(){
+            }, 1000);
         });
-        
-        localStorage.removeItem('mail');
+    };
 
-    }
-}
+    // Inserting text into elements
+    matchName.appendChild(nameTxt);
+    matchAge.appendChild(ageTxt);
+    deleteMatch.appendChild(buttonTxt);
 
+    // Inserting elements into div
+    holder.appendChild(matchName);
+    holder.appendChild(matchAge);
+    holder.appendChild(deleteMatch);
 
-
+    // Insert the final match display
+    document.body.appendChild(holder);
+};
